9ddbe1a763f31acd3d542cdfa887b906577769c0
Gustav Kristensen <gustavkrist@gmail.com>
2021-12-15 19:45:58 +0100

    finally solved day 12

--- day12/day12_nx.py
+++ day12/day12_nx.py
@@ -1,5 +1,7 @@
-<<<<<<< HEAD
+import sys
 import networkx as nx
+import pprint
+pprint = pprint.pprint
 
 
 def generate_graph(data):
@@ -9,13 +11,12 @@ def generate_graph(data):
         G.add_edge(node1, node2)
     for node in G.nodes:
         if node not in ['start', 'end']:
-            if 65 <= ord(node) <= 90:
+            if 65 <= ord(node[0]) <= 90:
                 G.nodes[node]['size'] = 'big'
             else:
                 G.nodes[node]['size'] = 'small'
         else:
             G.nodes[node]['size'] = None
-        G.nodes[node]['visited'] = False
     return G
 
 
@@ -23,94 +24,43 @@ def size(G, node):
     return G.nodes[node]['size']
 
 
-def visited(G, node):
-    return G.nodes[node]['visited']
+def dfs(G, node, visited, path, paths, part2, twice=False):
+    if node == 'end':
+        paths.append(path + [node])
+        return
+    if size(G, node) == 'small':
+        visited[node] += 1
+        if visited[node] > 1 and part2:
+            twice = True
+    for neighbor in G[node]:
+        if visited[neighbor] > 0 and not twice and part2 and neighbor != 'start':
+            paths.append(path + [node])
+            dfs(G, neighbor, visited, path + [node], paths, part2, twice)
+        elif visited[neighbor] < 1 and neighbor != 'start':
+            paths.append(path + [node])
+            dfs(G, neighbor, visited, path + [node], paths, part2, twice)
+    visited[node] -= 1
+    twice = False
 
 
-def find_paths(G):
-    H = G.copy()
-    H.clear_edges()
-    H = H.to_directed()
-    nodes = ['start']
-    while nodes:
-        neighbors = list()
-        for node in nodes:
-            if node == 'end':
-                pass
-            else:
-                if size(G, node) != 'big':
-                    G.nodes[node]['visited'] = True
-                for neighbor in G[node]:
-                    if not visited(G, neighbor):
-                        # if size(G, neighbor) == 'big':
-                        #     nn_len = len(G[neighbor])
-                        #     nn_visited = 0
-                        #     for nn in G[neighbor]:
-                        #         if visited(G, nn):
-                        #             nn_visited += 1
-                        #         if size(G, nn) == 'big':
-                        #             nn_len -= 1
-                        #     if nn_visited < nn_len:
-                        #         H.add_edge(node, neighbor)
-                        #         neighbors.append(neighbor)
-                        # else:
-                        H.add_edge(node, neighbor)
-                        neighbors.append(neighbor)
-        nodes = neighbors
-    print(H.edges)
-    path_count = 0
-    for path in nx.all_simple_paths(G, 'start', 'end'):
-        print(path)
-
-
-def main():
-    with open("sample.txt") as f:
-        data = list(map(str.rstrip, f.readlines()))
-        G = generate_graph(data)
-    # print(list(nx.all_simple_paths(G, 'start', 'end')))
-    print(find_paths(G))
-=======
-import sys
-from itertools import chain
-import pprint
-pprint = pprint.pprint
-
-
-def create_adj_dict(file):
-    adj_dict = {}
-    for line in file:
-        nodes = line.rstrip().split('-')
-        for node1, node2 in zip(nodes, reversed(nodes)):
-            if node1 not in adj_dict and node2 != 'start':
-                adj_dict[node1] = {node2}
-            elif node2 != 'start':
-                adj_dict[node1].add(node2)
-    return adj_dict
-
-
-def big(node):
-    if len(node) == 1:
-        if 65 <= ord(node) <= 90:
-            return True
-        else:
-            return False
-    else:
-        return False
-
-
-def find_paths(node, adj_dict, visited):
-    pass
-
-
-def main():
-    adj_dict = create_adj_dict(sys.stdin)
-    visited = {n: False for n in adj_dict.keys()}
-    print(adj_dict)
-    adj_dict = {'A': {'c', 'b', 'end'}, 'c': set(), 'b': set()}
+def find_paths(G, part2=False):
+    visited = {node: 0 for node in G.nodes}
+    path = []
     paths = []
-    find_paths('A', adj_dict, visited)
-    pprint(paths)
->>>>>>> bc62c2db3efcec05bd974a56aff9837bd94fc581
+    dfs(G, 'start', visited, path, paths, part2)
+    start_end_paths = [p for p in paths if p[-1] == 'end']
+    return len(start_end_paths)
+
+
+def main():
+    data = list(map(str.rstrip, sys.stdin.readlines()))
+    G = generate_graph(data)
+    result = []
+    with open('result.txt') as f:
+        for line in f:
+            result.append(tuple(line.rstrip().split(',')))
+    print(find_paths(G))
+    print(find_paths(G, part2=True))
 
 
 if __name__ == "__main__":
